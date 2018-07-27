7321b2b75c5f540c4b16d91fa6c6ed1821fdec97
bryan0513 <bryan.d.gonzalez@student.uncommonhs.org>
2018-07-27 12:50:30 -0700

    new

--- ball.js
+++ ball.js
@@ -1,4 +1,8 @@
-let ball ='blue';
+let colors=['red','blue','green']
+Math.random(colors)
+
+
+let ball =colors;// set color 
 //lines 6 & 7
 //says hey: everyone in ball.js
 //theres two global variables called
@@ -23,13 +27,20 @@ function draw(){
     fill(ball)
     background(75);
     ellipse(xCoord,yCoord,100);//circle
-    if (xCoord>495){
+    if (xCoord>499){
         xDirect*=-1
     }else if(xCoord<5){
         xDirect*=-1
     }
     xCoord+=xDirect;
 
-    
-    
+    //Y axis
+        fill(ball)
+        background(75);
+        ellipse(xCoord,yCoord,100);
+            if(yCoord>499){
+                xDirect*=-1
+            }else if(yCoord<5){
+                yDirect*=-1
+            }
 }
